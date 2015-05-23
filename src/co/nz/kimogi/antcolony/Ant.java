4c8a12be172b81ce5eda0eb0012b20d0b36fe630
kimogi <kimogi@teknique.com>
2015-05-23 22:44:00 +1200

    line follow added

--- src/co/nz/kimogi/antcolony/Ant.java
+++ src/co/nz/kimogi/antcolony/Ant.java
@@ -1,16 +1,32 @@
 package co.nz.kimogi.antcolony;
 
+import java.awt.Color;
 import java.awt.Rectangle;
 
 public class Ant
 {
+	public int id;
 	public Rectangle rect;
-	public boolean isFollower = false;
+	public Color color;
 	public boolean isLeader = false;
+	public boolean isFollower = false;
 	
-	public Ant(Rectangle rect, boolean isLeader)
+	public Ant(int id, Rectangle rect, boolean isLeader)
 	{
 		this.rect = rect;
 		this.isLeader = isLeader;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		Ant ant = (Ant)obj;
+		return id == ant.id;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return id;
 	}
 }
--- src/co/nz/kimogi/antcolony/AntColony.java
+++ src/co/nz/kimogi/antcolony/AntColony.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -20,20 +22,21 @@ public class AntColony extends JFrame
 	{
 		private static final long serialVersionUID = -9027962180934835275L;
 		public final static int NUM_ANTS = 100;
-		public final static int WIDTH = 400; // pixels
-		public final static int HEIGHT = 400; // pixels
+		public final static int WIDTH = 400;
+		public final static int HEIGHT = 400;
 
-		private final static int SZ = 4; // ant side length, pixels
+		private final static int SZ = 4;
 
-		public Ant[] ants = new Ant[NUM_ANTS];
+		public LinkedList<Ant> ants = new LinkedList<Ant>();
+		public LinkedList<Ant> followers = new LinkedList<Ant>();
 
 		public AntPanel()
 		{
 			Random rand = new Random();
-			ants[0] = new Ant(new Rectangle(rand.nextInt(WIDTH), rand.nextInt(HEIGHT), SZ, SZ), true);
-			for (int i = 1; i < ants.length; i++)
+			ants.addLast(new Ant(0, new Rectangle(rand.nextInt(WIDTH), rand.nextInt(HEIGHT), SZ, SZ), true));
+			for (int i = 1; i < NUM_ANTS; i++)
 			{
-				ants[i] = new Ant(new Rectangle(rand.nextInt(WIDTH), rand.nextInt(HEIGHT), SZ, SZ), false);
+				ants.addLast(new Ant(i, new Rectangle(rand.nextInt(WIDTH), rand.nextInt(HEIGHT), SZ, SZ), false));
 			}
 
 			setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -42,33 +45,30 @@ public class AntColony extends JFrame
 
 		private void checkID(int id)
 		{
-			if (id < 0 || id >= ants.length)
+			if (id < 0 || id >= NUM_ANTS)
+			{
 				throw new IllegalArgumentException("Bad ant id: " + id);
+			}
 		}
 
-		public synchronized Point getAntLocation(int id)
+		public synchronized void setAntLocation(Ant ant, int x, int y)
 		{
-			checkID(id);
-			Ant ant = ants[id];
-			return new Point(ant.rect.x, ant.rect.y);
-		}
-
-		public synchronized void setAntLocation(int id, int x, int y)
-		{
-			checkID(id);
+			checkID(ant.id);
 			if (x < 0)
+			{
 				x = (Math.abs(x) / WIDTH + 1) * WIDTH + x;
-
+			}
 			if (y < 0)
+			{
 				y = (Math.abs(y) / HEIGHT + 1) * HEIGHT + y;
-
-			ants[id].rect.setLocation(x % WIDTH, y % HEIGHT);
+			}
+			ant.rect.setLocation(x % WIDTH, y % HEIGHT);
 		}
 
-		public synchronized void moveAnt(int id, int dx, int dy)
+		public synchronized void moveAnt(Ant ant, int dx, int dy)
 		{
-			checkID(id);
-			setAntLocation(id, ants[id].rect.x + dx, ants[id].rect.y + dy);
+			checkID(ant.id);
+			setAntLocation(ant, ant.rect.x + dx, ant.rect.y + dy);
 		}
 
 		@Override
@@ -77,21 +77,12 @@ public class AntColony extends JFrame
 			Graphics2D g2 = (Graphics2D) g;
 			g2.clearRect(0, 0, WIDTH, HEIGHT);
 
-			for (Ant ant : ants)
+			Iterator<Ant> iter = ants.iterator();
+			while (iter.hasNext())
 			{
-				if (ant.isLeader)
-				{
-					g2.setColor(Color.BLUE);
-					g2.fill(ant.rect);
-				} else if (ant.isFollower)
-				{
-					g2.setColor(Color.GREEN);
-					g2.fill(ant.rect);
-				} else
-				{
-					g2.setColor(Color.RED);
-					g2.fill(ant.rect);
-				}
+				Ant ant = iter.next();
+				g2.setColor(ant.color);
+				g2.fill(ant.rect);
 			}
 		}
 	}
@@ -109,52 +100,82 @@ public class AntColony extends JFrame
 	private double theta = -Math.PI;
 	private Point center = new Point(AntPanel.WIDTH / 2, AntPanel.HEIGHT / 2);
 	private int radius = 100;
-	private int hookUpRadius = 20;
+	private int hookUpRadius = 10;
+	private int keepRadius = 5;
 
 	public void updateLoop()
 	{
 		while (keepRunning)
 		{
-			for (int i = 0; i < AntPanel.NUM_ANTS; i++)
+			Ant leader = antPanel.ants.getFirst();
+			theta = theta + Math.PI / 36;
+			if (theta > Math.PI)
 			{
-				Ant ant = antPanel.ants[i];
-				Ant leader = antPanel.ants[0];
-				antPanel.moveAnt(i, rand.nextInt(5) - 2, rand.nextInt(5) - 2);
+				theta = -Math.PI;
+			}
+			leader.rect.x = center.x + (int) (radius * Math.cos(theta));
+			leader.rect.y = center.y + (int) (radius * Math.sin(theta));
+			leader.color = Color.BLUE;
 
-				if (ant.isLeader)
-				{
-					theta = theta + Math.PI / 36;
-					if (theta > Math.PI)
-					{
-						theta = -Math.PI;
-					}
-					ant.rect.x = center.x + (int) (radius * Math.cos(theta));
-					ant.rect.y = center.y + (int) (radius * Math.sin(theta));
-				} 
-				else
-				{
-					if (Math.abs(ant.rect.x - leader.rect.x) <= hookUpRadius && Math.abs(ant.rect.y - leader.rect.y) <= hookUpRadius)
-					{
-						antPanel.ants[i].isFollower = true;
-					}
+			Ant measureAnt = antPanel.followers.isEmpty() ? leader : antPanel.followers.getLast();
 
-					if (ant.isFollower)
+			Iterator<Ant> iter = antPanel.ants.iterator();
+			while (iter.hasNext())
+			{
+				Ant ant = iter.next();
+				if (!ant.isLeader && !ant.isFollower)
+				{
+					if (calcDistance(ant, measureAnt) > hookUpRadius)
 					{
-						// TODO: follow
+						int dx = rand.nextInt(5) - 2;
+						int dy = rand.nextInt(5) - 2;
+						antPanel.moveAnt(ant, dx, dy);
+						ant.color = Color.RED;
 					}
+					else
+					{
+						ant.color = Color.GREEN;
+						antPanel.followers.addLast(ant);
+						ant.isFollower = true;
+					}
+				}				
+			}
+
+			measureAnt = leader;
+			iter = antPanel.followers.iterator();
+			while (iter.hasNext())
+			{
+				Ant follower = iter.next();
+				int x2 = follower.rect.x;
+				int y2 = follower.rect.y;
+				int x1 = measureAnt.rect.x;
+				int y1 = measureAnt.rect.y;
+				int distance = calcDistance(follower, measureAnt);
+			
+				if (distance > keepRadius)
+				{
+					int dx = (int) (x1 - x2) * (distance - keepRadius)/distance;
+					int dy = (int) (y1 - y2) * (distance - keepRadius)/distance;
+					antPanel.moveAnt(follower, dx, dy);
 				}
+				measureAnt = follower;
 			}
 			antPanel.repaint();
 
 			try
 			{
 				Thread.sleep(50);
-
-			} catch (Exception ex)
+			}
+			catch (Exception ex)
 			{
 				ex.printStackTrace();
 			}
 		}
+	}
+
+	private int calcDistance(Ant ant1, Ant ant2)
+	{
+		return (int) Math.sqrt((ant1.rect.x - ant2.rect.x) * (ant1.rect.x - ant2.rect.x) + (ant1.rect.y - ant2.rect.y) * (ant1.rect.y - ant2.rect.y));
 	}
 
 	public void startAnimation()
